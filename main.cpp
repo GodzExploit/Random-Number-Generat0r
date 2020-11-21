59a37f3d693397e7d9e61c5d115114d5b3a73473
GodzzExploit <ssujan2418076@gmail.com>
2020-11-21 15:24:23 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <sstream> 
+using namespace std;
+
+//Random Number Generator
+//ex: (0,1,0,1)
+inline int a() {
+		int z = rand() % 2;
+		return z;
+}
+
+int main() {
+	//generates random numbers
+	srand(time(0));
+
+	for (int i = 0; i < 5; ++i) {
+	 cout << a() << "," << a() << "," << a() << "," << a() << " ";
+	}
+}
+
+
+
