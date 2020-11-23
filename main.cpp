0fc1246d76e9b0591d5565b91cbde8e3da1d121b
GodzzExploit <ssujan2418076@gmail.com>
2020-11-22 23:25:25 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,23 +3,28 @@
 #include <string>
 #include <sstream> 
 using namespace std;
-
 //Random Number Generator
 //ex: (0,1,0,1)
-inline int a() {
-		int z = rand() % 2;
-		return z;
+
+//prints the random number
+int Random(int a, int b, int c, int d) {
+	cout << a << "," << b << "," << c << "," << d << " " << endl;
+	return a, b, c, d;
+}
+//sets to print between 0's and 1's only.
+int a() {
+	int z = rand() % 2;
+	return z;
 }
 
 int main() {
-	//generates random numbers
+	//generates random numbers everytime
 	srand(time(0));
-        
-	//loop for printing more then 1 row of numbers
+
+	//loop for how many times you want to print
 	for (int i = 0; i < 5; ++i) {
-	 cout << a() << "," << a() << "," << a() << "," << a() << " " << endl;
+		Random(a(), a(), a(), a());
 	}
+	
+	
 }
-
-
-
